6f28b92efddfe0b8dec7204088501e6ec7c086f5
Iulian <iulian@jcode.solutions>
2020-09-23 17:46:07 +0300

    refactor

--- src/app/BagPage/bucket/bucket.component.ts
+++ src/app/BagPage/bucket/bucket.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bucket.component.css']
 })
 export class BucketComponent implements OnInit {
-
+  quantity: number = 0;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  value(sing: string): void {
+    switch (sing) {
+      case 'minus': this.quantity--;
+                    break;
+      case 'plus': this.quantity++;
+                   break;
+    }
   }
 
 }
